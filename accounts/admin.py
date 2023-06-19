1871b8399a47a45d64289e8cdd1e2c35b5c7d614
Ikhide Augustine <augustineimoleme@gmail.com>
2023-06-19 21:05:23 +0100

    added media root for images collection

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import User, UserProfile
+from .models import User, UserProfile, Friendship
 
 admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.register(Friendship)
