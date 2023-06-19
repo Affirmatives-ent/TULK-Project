c03d5d1217a5a57faf89c9d902cc253940edce72
Ikhide Augustine <augustineimoleme@gmail.com>
2023-06-19 03:38:20 +0100

    change posts url structure

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -183,6 +183,9 @@ MIN_PASSWORD_LENGTH = 8
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIR = (os.path.join(BASE_DIR, "static"),)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
