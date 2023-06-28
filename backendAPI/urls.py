b26ff12b938e8f27be9608b66609ddfff6779500
Ikhide Augustine <augustineimoleme@gmail.com>
2023-06-28 09:49:43 +0100

    Added article and media publishing

--- backendAPI/urls.py
+++ backendAPI/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     # path('auth/', include('djoser.urls')),
     # path('auth/', include('djoser.urls.jwt')),
     path("", include("accounts.urls", namespace="accounts")),
+    path("", include("articles.urls", namespace="articles")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
