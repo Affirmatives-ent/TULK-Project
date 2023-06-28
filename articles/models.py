294f2bce5d5534687711894451722ae3407edcf9
Ikhide Augustine <augustineimoleme@gmail.com>
2023-06-28 09:41:34 +0100

    Added article and media publishing

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.conf import settings
 
 
 class MediaFile(models.Model):
     caption = models.CharField(max_length=100)
     file = models.FileField(upload_to='media/')
-    published_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    published_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     published_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
@@ -32,7 +34,8 @@ class Article(models.Model):
     media_files = models.ForeignKey(
         MediaFile, on_delete=models.SET_NULL, null=True, blank=True, related_name='articles')
     categories = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='draft')
     published_date = models.DateTimeField(null=True, blank=True)
