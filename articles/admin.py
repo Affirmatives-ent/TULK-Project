a3061a0de1d3e299ec6a2f8041659caf58bce6ec
Ikhide Augustine <augustineimoleme@gmail.com>
2023-06-28 10:03:43 +0100

    Added article and media publishing

--- articles/admin.py
+++ articles/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from models import MediaFile, Article
-
-# Register your models here.
+from .models import MediaFile, Article
 
 admin.site.register(MediaFile)
 admin.site.register(Article)
