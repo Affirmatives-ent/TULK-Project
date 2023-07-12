ed03b836c9d0fcc2f6a5bed2e52aecba01cc841d
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-12 22:42:24 +0100

    Added allow-all-access for cors headers

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -162,6 +162,8 @@ AUTHENTICATION_BACKENDS = [
     'accounts.authentications.CustomAuthBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000",
