6a93ab9d9666ad9c23015dec7b1bdc63ae6a5745
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-16 15:01:30 +0100

    I updated the posts app completely

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Post, Comment
+from .models import Post, Comment, Like, Share
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Share)
