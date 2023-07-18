dd563ba1f9cb52630902d6555417443cd31f3126
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-18 21:04:20 +0100

    I update and customized the expired token error

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -172,7 +172,7 @@ CORS_ORIGIN_WHITELIST = (
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
