97acfcc63b4b233eb2dc5ca9a2b0d28069d255a0
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-21 23:13:35 +0100

    Updated the search endpoint

--- user_groups/urls.py
+++ user_groups/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
          name='create_conversation_group'),
     path('groups/<uuid:group_id>/invite/',
          views.InviteUserToGroup.as_view(), name='invite_user_to_group'),
-    path('friends/search/', views.FriendSearch.as_view(), name='friend_search'),
     path('groups/<uuid:group_id>/invitation/',
          views.AcceptOrRejectInvitation.as_view(), name='accept_or_reject_invitation'),
     path('groups/<uuid:group_id>/chats/',
