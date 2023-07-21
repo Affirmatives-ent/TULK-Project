1756257f87b3bc79a3389726ff4b2cfa6e1c6bf6
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-21 12:28:25 +0100

    Updated the search endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -450,7 +450,7 @@ class SearchAPIView(generics.ListAPIView):
 
         # Search for posts or articles by title or category
         post_results = Post.objects.filter(
-            Q(authur__icontains=search_query) |
+            Q(author__icontains=search_query) |
             Q(content__icontains=search_query)
         )
         results.extend(post_results)
