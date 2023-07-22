3ef9af7fc852d5ac6e17547c14c7072d35f6aeee
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-22 05:08:48 +0100

    Updated the articles endpoints

--- articles/urls.py
+++ articles/urls.py
@@ -5,10 +5,10 @@ app_name = "articles"  # Add the 'app_name' here
 
 urlpatterns = [
     # Admin URLs
-    path('admin/articles/', AdminArticleListView.as_view(),
-         name='admin-article-list'),
-    path('admin/articles/<uuid:pk>/', AdminArticleDetailView.as_view(),
-         name='admin-article-detail'),
+    path('editor/articles/', AdminArticleListView.as_view(),
+         name='editor-article-list'),
+    path('editor/articles/<uuid:pk>/', AdminArticleDetailView.as_view(),
+         name='editor-article-detail'),
 
     # User URLs
     path('articles/', UserArticleListView.as_view(), name='user-article-list'),
