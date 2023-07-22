d9fb2a53deb1c052c22191b3b36ee4724129957c
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-22 04:40:15 +0100

    Updated the articles endpoints

--- articles/models.py
+++ articles/models.py
@@ -39,6 +39,7 @@ class Article(models.Model):
 
 
 class MediaFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file = models.FileField(upload_to='media/', null=True, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
