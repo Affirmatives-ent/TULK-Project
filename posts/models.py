50d386e770327302431c4ce6f92fba8f0ff822d2
Ikhide Augustine <augustineimoleme@gmail.com>
2023-07-30 23:40:44 +0100

    Restructured the post app models, serializers and views

--- posts/models.py
+++ posts/models.py
@@ -1,74 +1,3 @@
-# from django.contrib.auth import get_user_model
-# from django.db import models
-# import uuid
-
-# User = get_user_model()
-
-
-# class Post(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     content = models.TextField(blank=True)
-#     files = models.ManyToManyField('File', blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     author = models.ForeignKey(
-#         User, on_delete=models.CASCADE, related_name='posts', to_field='id')
-
-#     class Meta:
-#         ordering = ["-created_at"]
-
-#     def __str__(self):
-#         return self.content[:20]
-
-
-# class File(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     file = models.FileField(upload_to='post_files/')
-#     uploaded_at = models.DateTimeField(auto_now_add=True)
-
-#     class Meta:
-#         ordering = ["-uploaded_at"]
-
-#     def __str__(self):
-#         return "Updloaded"
-
-
-# class Like(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
-#     post = models.ForeignKey(
-#         Post, on_delete=models.CASCADE, related_name='likes', to_field='id')
-#     liked_at = models.DateTimeField(auto_now_add=True)
-
-#     class Meta:
-#         ordering = ["-liked_at"]
-
-#     def __str__(self):
-#         return f"{self.user} liked {self.post.author}\'s post"
-
-
-# class Comment(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
-#     post = models.ForeignKey(
-#         Post, on_delete=models.CASCADE, related_name='comments', to_field='id')
-#     content = models.TextField()
-
-#     commented_at = models.DateTimeField(auto_now_add=True)
-
-#     class Meta:
-#         ordering = ["-commented_at"]
-
-#     def __str__(self):
-#         return self.content[:20]
-
-
-# class Share(models.Model):
-#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
-#     post = models.ForeignKey(
-#         Post, on_delete=models.CASCADE, related_name='shares', to_field='id')
-
-
 from django.contrib.auth import get_user_model
 from django.db import models
 import uuid
@@ -79,6 +8,7 @@ User = get_user_model()
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     content = models.TextField(blank=True)
+    files = models.ManyToManyField('File', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts', to_field='id')
@@ -91,18 +21,15 @@ class Post(models.Model):
 
 
 class File(models.Model):
-    id = models.UUIDField(primary_key=True, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     file = models.FileField(upload_to='post_files/')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='files')
-
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ["-uploaded_at"]
 
     def __str__(self):
-        return "Uploaded"
+        return "Updloaded"
 
 
 class Like(models.Model):
@@ -116,7 +43,7 @@ class Like(models.Model):
         ordering = ["-liked_at"]
 
     def __str__(self):
-        return f"{self.user} liked {self.post.author}'s post"
+        return f"{self.user} liked {self.post.author}\'s post"
 
 
 class Comment(models.Model):
@@ -140,10 +67,83 @@ class Share(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name='shares', to_field='id')
-    shared_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ["-shared_at"]
 
-    def __str__(self):
-        return f"{self.user} shared {self.post.author}'s post"
+# from django.contrib.auth import get_user_model
+# from django.db import models
+# import uuid
+
+# User = get_user_model()
+
+
+# class Post(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     content = models.TextField(blank=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     author = models.ForeignKey(
+#         User, on_delete=models.CASCADE, related_name='posts', to_field='id')
+
+#     class Meta:
+#         ordering = ["-created_at"]
+
+#     def __str__(self):
+#         return self.content[:20]
+
+
+# class File(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     file = models.FileField(upload_to='post_files/')
+#     post = models.ForeignKey(
+#         Post, on_delete=models.CASCADE, related_name='files')
+
+#     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         ordering = ["-uploaded_at"]
+
+#     def __str__(self):
+#         return "Uploaded"
+
+
+# class Like(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
+#     post = models.ForeignKey(
+#         Post, on_delete=models.CASCADE, related_name='likes', to_field='id')
+#     liked_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         ordering = ["-liked_at"]
+
+#     def __str__(self):
+#         return f"{self.user} liked {self.post.author}'s post"
+
+
+# class Comment(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
+#     post = models.ForeignKey(
+#         Post, on_delete=models.CASCADE, related_name='comments', to_field='id')
+#     content = models.TextField()
+
+#     commented_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         ordering = ["-commented_at"]
+
+#     def __str__(self):
+#         return self.content[:20]
+
+
+# class Share(models.Model):
+#     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
+#     post = models.ForeignKey(
+#         Post, on_delete=models.CASCADE, related_name='shares', to_field='id')
+#     shared_at = models.DateTimeField(auto_now_add=True)
+
+#     class Meta:
+#         ordering = ["-shared_at"]
+
+#     def __str__(self):
+#         return f"{self.user} shared {self.post.author}'s post"
--- posts/urls.py
+++ posts/urls.py
@@ -1,24 +1,27 @@
 
 from django.urls import path
+import uuid
 from .views import (
     PostListCreateView,
     PostRetrieveUpdateDestroyAPIView,
-    UserPostsAPIView,
     CommentListCreateAPIView,
-    LikeToggleAPIView,
+    LikeListCreateAPIView,
     ShareListCreateAPIView,
+    UserPostsAPIView
 )
+
+app_name = 'posts'
 
 urlpatterns = [
     path('posts/', PostListCreateView.as_view(), name='post-list-create'),
     path('posts/<uuid:pk>/', PostRetrieveUpdateDestroyAPIView.as_view(),
          name='post-retrieve-update-destroy'),
     path('users/<uuid:user_id>/posts/',
-         UserPostsAPIView.as_view(), name='user-posts'),
+         UserPostsAPIView.as_view(), name='user_posts'),
     path('posts/<uuid:post_id>/comments/',
-         CommentListCreateAPIView.as_view(), name='comment-list-create'),
+         CommentListCreateAPIView.as_view(), name='post-comment-list-create'),
     path('posts/<uuid:post_id>/like/',
-         LikeToggleAPIView.as_view(), name='like-list-create'),
+         LikeListCreateAPIView.as_view(), name='post-like'),
     path('posts/<uuid:post_id>/share/',
-         ShareListCreateAPIView.as_view(), name='share-list-create'),
+         ShareListCreateAPIView.as_view(), name='post-share'),
 ]
