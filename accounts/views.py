47684440c1717d0462c760f114e8f7f89e169fe2
Ikhide Augustine <augustineimoleme@gmail.com>
2023-08-10 09:16:12 +0100

    added a friend list to the UserProfile endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -319,6 +319,7 @@ class UserProfileDetailAPIView(generics.RetrieveUpdateAPIView):
         user_friends_data = serializers.UserFriendsSerializer(
             friends, many=True).data
         serializer.data['user_friends'] = user_friends_data
+        print(serializer.data)
 
         return Response(serializer.data)
 
