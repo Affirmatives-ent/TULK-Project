425e1787aa20864d2076862de8f4d667394de65c
Ikhide Augustine <augustineimoleme@gmail.com>
2023-08-22 19:23:25 +0100

    Added endpoint to retrieve all media files of a user

--- accounts/views.py
+++ accounts/views.py
@@ -570,7 +570,7 @@ class UserMediaFilesView(generics.ListAPIView):
         # Retrieve the user based on user_id
         user = get_object_or_404(User, id=user_id)
 
-        print(user)
+        print(user.id)
 
         # List to store media files
         media_files = []
