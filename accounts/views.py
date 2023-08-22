8d72544856c0b4d3dc0e2cfeb1287426f7d7efc3
Ikhide Augustine <augustineimoleme@gmail.com>
2023-08-22 19:59:26 +0100

    Added endpoint to retrieve all media files of a user

--- accounts/views.py
+++ accounts/views.py
@@ -570,7 +570,7 @@ class UserMediaFilesView(generics.ListAPIView):
         # Retrieve the user based on user_id
         user = get_object_or_404(User, id=user_id)
 
-        print(user.id)
+        print(f'This is the user id: {user.id}')
 
         # List to store media files
         media_files = []
@@ -583,14 +583,14 @@ class UserMediaFilesView(generics.ListAPIView):
 
         # Add media files from articles
         articles = Article.objects.filter(author=user.id)
-        print(articles)
+        print(f'This is the article id: {articles}')
         for article in articles:
             if article.featured_image:
                 media_files.append(article.featured_image)
-                print(article.files.all())
+                print(f'This is the list of all the article files: {article.files.all()}')
             # Add other media files associated with the article
             media_files.extend(article.files.all())
-            print(media_files)
+            print(f'This is the whole media files list: {media_files}')
 
         # Add media files from posts
         posts = Post.objects.filter(author=user.id)
