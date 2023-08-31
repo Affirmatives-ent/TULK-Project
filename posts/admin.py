0f86f31892203c619b2e66dd54e0208c225123cb
Ikhide Augustine <augustineimoleme@gmail.com>
2023-08-31 07:33:44 +0100

    Updated the admin files to display the information in a more presentable way

--- posts/admin.py
+++ posts/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import Post, Comment, Like, Share
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'content')
+    list_filter = ('author')
+
+
+admin.site.register(Post, PostAdmin)
+
+
 admin.site.register(Comment)
 admin.site.register(Like)
 admin.site.register(Share)
