22776b5a6886ca22eaae37b10f10f22536e44dc9
Ikhide Augustine <augustineimoleme@gmail.com>
2023-10-09 10:21:19 +0100

    I set debug to false

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET']
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
-DEBUG = True
+DEBUG = False
 # SECRET_KEY = config("SECRET_KEY")
 
 # # SECURITY WARNING: don't run with debug turned on in production!
