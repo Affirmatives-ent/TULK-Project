2ee3ef59114f3c07d48fffb0d338545749dc998f
Ikhide Augustine <augustineimoleme@gmail.com>
2023-10-11 13:50:45 +0100

    added cloudinary credentials

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -299,7 +299,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': "hyklbuwof",
+    'CLOUD_NAME': "Tulk",
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
