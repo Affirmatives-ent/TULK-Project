71662d69d532b65b78247ac02a52b5a26cc35c0e
Ikhide Augustine <augustineimoleme@gmail.com>
2023-10-25 22:19:07 +0100

    Modified the groups urls to fetch individual group

--- backendAPI/settings.py
+++ backendAPI/settings.py
@@ -257,7 +257,7 @@ CORS_ORIGIN_WHITELIST = (
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
